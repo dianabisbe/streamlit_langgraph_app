8537b190e3c110b136fed7a77d03f7cfc5dca28c
dianabisbe <diana.bisbe@gmail.com>
2024-09-05 23:21:56 +0200

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+"""Module providing the UI for the LangGraph AI Agent using Streamlit."""
+
+import streamlit as st
+import requests
+
+st.title('LangGraph AI Agent')
+
+PROMPT_INPUT = '''Pregúntame sobre artículos de wikipedia
+                y tus documentos de runiones'''
+user_input = st.text_input(PROMPT_INPUT, "")
+
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+
+for message in st.session_state['history']:
+    st.write(f"You: {message['user']}")
+    st.write(f"Agent: {message['agent']}")
+
+if user_input:
+    with st.spinner("Pensando..."):
+        response = requests.post("http://localhost:8000/invoke",
+                         json={"query": user_input},
+                         timeout=10)
+
+        agent_response = response.json()['response']
+
+        st.session_state['history'].append({"user": user_input, "agent": agent_response})
+
+        st.write(f"You: {user_input}")
+        st.write(f"Agent: {agent_response}")
